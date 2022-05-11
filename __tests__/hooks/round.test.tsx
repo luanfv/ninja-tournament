2dd36698cde4a80697f28ba5e6f6617312d06e41
luanfv <luanfrancavieira@gmail.com>
2022-05-11 17:57:13 -0300

    refactor: tests

--- __tests__/hooks/round.test.tsx
+++ __tests__/hooks/round.test.tsx
@@ -1,13 +1,12 @@
 import { renderHook } from '@testing-library/react-hooks';
 
-import { useRound } from '../../src/hooks';
+import { useRound } from '../../src/hooks/round';
 
 const shinobis = [
   {
     chakra: 10,
     id: 7,
-    image:
-      'https://firebasestorage.googleapis.com/v0/b/naruto-shuriken.appspot.com/o/kakashi.png?alt=media&token=95d4e2b7-28b9-43cc-bfd9-eb0402cdedab',
+    image: '',
     name: 'Kakashi',
     power: 10,
     technique: 18,
@@ -15,8 +14,7 @@ const shinobis = [
   {
     chakra: 4,
     id: 11,
-    image:
-      'https://firebasestorage.googleapis.com/v0/b/naruto-shuriken.appspot.com/o/ino.webp?alt=media&token=05e85fe5-9ef9-443b-af65-a9535411187a',
+    image: '',
     name: 'Ino',
     power: 2,
     technique: 10,
@@ -24,8 +22,7 @@ const shinobis = [
   {
     chakra: 16,
     id: 1,
-    image:
-      'https://firebasestorage.googleapis.com/v0/b/naruto-shuriken.appspot.com/o/naruto.jpeg?alt=media&token=08633011-2f85-4df1-a2a5-d6ee015ac98a',
+    image: '',
     name: 'Naruto',
     power: 8,
     technique: 6,
@@ -33,8 +30,7 @@ const shinobis = [
   {
     chakra: 6,
     id: 12,
-    image:
-      'https://firebasestorage.googleapis.com/v0/b/naruto-shuriken.appspot.com/o/neji.png?alt=media&token=1e848729-f44d-43ad-9375-fde6fae6e69b',
+    image: '',
     name: 'Neji',
     power: 8,
     technique: 12,
@@ -45,24 +41,25 @@ describe('Hook: useRound', () => {
   const [player1, player2] = shinobis;
   const { result } = renderHook(() => useRound());
 
-  test('onStartRound: Response length', () => {
-    expect(result.current.onStartRound(shinobis).length).toBe(2);
+  it('onStartRound: Should return a list with round information if it has a number for competitors', () => {
+    const response = result.current.onStartRound(shinobis);
+    const length = response.length;
+
+    expect(length).toBe(2);
+  });
+
+  it('onStartRound: Must fail if it has an odd number of competitors for the round', () => {
     expect(() => result.current.onStartRound([player1])).toThrow();
   });
 
-  test('onStartRound: Percentage', () => {
-    const player1Points = player1.power + player1.chakra + player1.technique;
-    const player2Points = player2.power + player2.chakra + player2.technique;
-
-    const player1WinPercentage =
-      (player1Points * 100) / (player1Points + player2Points);
-
+  it('onStartRound: Must know the winning percentage of the two competitors in the round', () => {
     const [response] = result.current.onStartRound([player1, player2]);
-    expect(response.player1.winPercentage).toBe(player1WinPercentage);
-    expect(response.player2.winPercentage).toBe(100 - player1WinPercentage);
+
+    expect(response.player1.winPercentage.toFixed(2)).toBe('70.37');
+    expect(response.player2.winPercentage.toFixed(2)).toBe('29.63');
   });
 
-  test('onStartRound: Champion', () => {
+  it('onStartRound: Must return round information and validate that the correct competitor has won', () => {
     const [player1Winner] = result.current.onStartRound([player1, player2], 50);
     expect(player1Winner.winner.id).toBe(player1.id);
 
@@ -70,7 +67,7 @@ describe('Hook: useRound', () => {
     expect(player2Winner.winner.id).toBe(player2.id);
   });
 
-  test('onStartAllRounds: Response length', () => {
+  it('onStartAllRounds: Must hold a tournament and validate if the size of the array returned is correct', () => {
     const response = result.current.onStartAllRounds(shinobis);
     const length1 = 2;
     const length1x1 = 2;
@@ -83,7 +80,11 @@ describe('Hook: useRound', () => {
     expect(() => result.current.onStartAllRounds([player1])).toThrow();
   });
 
-  test('onStartAllRounds: Champion', () => {
+  it('onStartAllRounds: Must fail if there are an odd number of competitors for the tournament', () => {
+    expect(() => result.current.onStartAllRounds([player1])).toThrow();
+  });
+
+  it('onStartAllRounds: Must return tournament information and validate that the correct competitor has won', () => {
     const [kakashi, ino, naruto, neji] = shinobis;
 
     const fiftyPercent = result.current.onStartAllRounds(shinobis, 50);
