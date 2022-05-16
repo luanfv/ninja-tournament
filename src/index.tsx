b8d2015da1b7b125adeb005ae8c3372bb58b4d8a
luanfv <luanfrancavieira@gmail.com>
2022-05-16 09:16:59 -0300

    feat: code config

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import CodePush from 'react-native-code-push';
 
 import { theme } from './settings';
 import { Providers } from './context';
 import { Routes } from './routes';
 
-export default () => (
+const App: React.FC = () => (
   // eslint-disable-next-line react-native/no-inline-styles
   <GestureHandlerRootView style={{ flex: 1 }}>
     <Providers>
@@ -24,3 +25,5 @@ export default () => (
     </Providers>
   </GestureHandlerRootView>
 );
+
+export default CodePush()(App);
