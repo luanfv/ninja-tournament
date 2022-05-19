fc94f44f26e69a16067d27828fa8b54f5ee1db60
luanfv <luanfrancavieira@gmail.com>
2022-05-19 14:47:59 -0300

    test: language

--- __tests__/hooks/language.test.tsx
+++ __tests__/hooks/language.test.tsx
@@ -5,32 +5,45 @@ import { useLanguage } from '@src/hooks/language';
 
 jest.mock('react-native', () => ({
   Platform: {
-    OS: 'android',
+    OS: jest.fn(),
   },
   NativeModules: {
     I18nManager: {
-      localeIdentifier: 'en_US',
+      localeIdentifier: jest.fn(),
     },
   },
 }));
 
-// mock.setMock(())
+const mockRN = RN as jest.Mocked<typeof RN>;
 
 describe('Hook: useLanguage', () => {
-  it('Android: en_US', async () => {
-    const { result } = renderHook(() => useLanguage());
+  it('Should be in en_US and return type = en_US', () => {
+    mockRN.NativeModules.I18nManager.localeIdentifier = 'en_US';
 
-    RN.NativeModules.I18nManager.localeIdentifier = 'en_US';
+    const { result } = renderHook(() => useLanguage());
 
     const response = result.current.language;
 
     expect(response.type).toBe('en_US');
   });
 
-  it('Android: pt_BR', async () => {
+  it('Should be in fr_FR and return type = en_US', () => {
+    mockRN.NativeModules.I18nManager.localeIdentifier = 'fr_FR';
+
     const { result } = renderHook(() => useLanguage());
 
     const response = result.current.language;
-    console.log(response.type);
+
+    expect(response.type).toBe('en_US');
+  });
+
+  it('Should be in pt_BR and return type = pt_BR', () => {
+    mockRN.NativeModules.I18nManager.localeIdentifier = 'pt_BR';
+
+    const { result } = renderHook(() => useLanguage());
+
+    const response = result.current.language;
+
+    expect(response.type).toBe('pt_BR');
   });
 });
